c9c8efffcdb962b82876f39a4d5f8a7a5c8c2cb5
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-03-07 23:32:10 -0300

    portfolio version 2

---
+++ precache-manifest.6fe12dd27cd9eaaebff7fc10ce319758.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc67e25156179c3660e825a4d37afbf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a5ec5dbb459e26a6a9f",
+    "url": "/static/css/main.28fb58fe.chunk.css"
+  },
+  {
+    "revision": "a70e5dca74c6248609b4",
+    "url": "/static/js/2.6a112644.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6a112644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5ec5dbb459e26a6a9f",
+    "url": "/static/js/main.29af9e7a.chunk.js"
+  },
+  {
+    "revision": "054186b95ddc0bcdd148",
+    "url": "/static/js/runtime-main.3fc34fa4.js"
+  },
+  {
+    "revision": "7b5fb88f12bec8143f00e21bc3222124",
+    "url": "/static/media/Roboto-Light.7b5fb88f.ttf"
+  },
+  {
+    "revision": "ac3f799d5bbaf5196fab15ab8de8431c",
+    "url": "/static/media/Roboto-Regular.ac3f799d.ttf"
+  },
+  {
+    "revision": "955eabff51b2e5b81cb90d2c640d32dc",
+    "url": "/static/media/background-image.955eabff.jpg"
+  }
+]);
